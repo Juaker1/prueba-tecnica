5427118bbc9fcdb2782da60910139946084e4d34
Joaquin Aguilar <joaquin.aguilar.ampuero@gmail.com>
2026-04-21 13:50:45 -0400

    Fix: bootstrap integrity y arreglo de barra de navegación responsiva
    Actualiza el hash de Subresource Integrity (SRI) del paquete Bootstrap en views/layouts/footer.php para que coincida con el archivo del CDN. Añade los atributos aria-controls, aria-expanded y aria-label al elemento navbar-toggler en views/layouts/header.php para mejorar la accesibilidad para las tecnologías de apoyo.

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -23,7 +23,7 @@
             <i class="bi bi-journal-text me-2"></i>Solicitudes
         </a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
